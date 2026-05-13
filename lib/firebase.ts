66d1306aabd1a4f6f7ba96e0fb61d8891acacb2f
Chen6005 <tobabyru3366@gmail.com>
2026-05-13 23:01:01 +0800

    Add Firebase Auth client initialization

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,4 +1,5 @@
 import { getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -15,3 +16,4 @@ export const firebaseApp = getApps().length
   : initializeApp(firebaseConfig);
 
 export const db = getFirestore(firebaseApp);
+export const auth = getAuth(firebaseApp);
